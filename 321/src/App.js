591e08583e528facb888a5b0d4b7b13e937a8b5f
Rufliex <danilkl255@mail.ru>
2023-04-23 14:30:57 +0300

    useState warmup

--- 321/src/App.js
+++ 321/src/App.js
@@ -1,11 +1,50 @@
-import React from 'react' 
-import TodoList from './todo/todoList';
+import React, { useState } from 'react' 
+
+function computeInitialCounter() {
+    console.log('Loading');
+    let num = 20;
+    return num;
+}
 
 function App() {
+  const [counter, setCounter] = useState(() => {
+    return computeInitialCounter();
+  })
+
+  const [state, setState] = useState({
+    title: 'Счетчик',
+    date: Date.now()
+  })
+
+  function increment() {
+      setCounter(counter + 1)
+  }
+
+  function decrement() {
+      if (counter === 0) {
+        alert('Нельзя меньше 0')
+      } else {
+        setCounter(counter - 1)
+      }
+  }
+  function updateTitle() {
+        setState ( prev => {
+            return {
+                ...prev, 
+                title: 'Название'
+        }
+    })
+  }
   return (
     <div className='wrapper'>
-      <h1>Здарова братья!</h1>
-      <TodoList></TodoList>йййй
+        <div className='menu'> 
+            <h1>Счетчик: {counter} </h1>
+            <button onClick = { increment } className='btn'>Добавить</button>
+            <button onClick = { decrement } className='btn'>Убрать</button>
+            <button onClick = {updateTitle} className='btn'>Изменить название</button>
+
+            <pre>{JSON.stringify(state, null, 2)}</pre>
+        </div>
     </div>
   );
 }
